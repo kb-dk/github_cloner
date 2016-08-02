648b7c2aa17208803ee82b004badf2a802f45425
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2016-08-02 11:20:25 +0200

    Updated travis config

--- travis_pypi_setup.py
+++ travis_pypi_setup.py
@@ -21,7 +21,7 @@ except:
     from urllib.request import urlopen
 
 
-GITHUB_REPO = 'blekinge/github_cloner'
+GITHUB_REPO = 'statsbiblioteket/github_cloner'
 TRAVIS_CONFIG_FILE = os.path.join(
     os.path.dirname(os.path.abspath(__file__)), '.travis.yml')
 
